483c7ba452d430a755fcf13cd8006d041e086bfa
Raja Soundaramourty <rajasoun@icloud.com>
2021-05-11 10:15:50 +0530

    Automation through JS Force

---
+++ jsforce/README.md
@@ -0,0 +1,7 @@
+# Getting Started
+
+## Setup
+
+1. `npm  install` to install node modules 
+1. `cp .env.sample` to `.env` and configure
+1. `node login.js` to login to salesforce and print details
---
+++ jsforce/login.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var jsforce = require("jsforce");
+var dotenv = require("dotenv").config();
+
+var loginUrl = process.env.LOGIN_URL;
+var username = process.env.USERNAME;
+var password = process.env.PASSWORD;
+var conn = new jsforce.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  loginUrl: loginUrl,
+  logLevel: process.env.DEBUG,
+});
+
+async function closeGracefully(signal) {
+  await fastify.close();
+  process.exit();
+}
+process.on("SIGINT", closeGracefully);
+
+
+conn.login(username, password, function (err, userInfo) {
+  if (err) {
+    return console.error(err);
+  }
+  // Now you can get the access token and instance URL information.
+  // Save them to establish connection next time.
+  console.log("Instance URL: " + conn.instanceUrl);
+  console.log("Access Token: " + conn.accessToken);
+  // logged in user property
+  console.log("User ID: " + userInfo.id);
+  console.log("Org ID: " + userInfo.organizationId);
+});
